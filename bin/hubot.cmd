d4b1d36cb118fdf8f618313e79afacbae8cf420c
Puncoz Nepal <puncoz138@gmail.com>
2019-01-10 00:17:07 +0545

    Typescript implmented

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-call npm install
+call yarn
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
---
+++ types/external/hubot-slack/index.d.ts
@@ -0,0 +1,16 @@
+declare module "hubot-slack" {
+  import {RTMClient} from "@slack/client"
+
+  interface Options {
+    token: string
+  }
+
+  class SlackClient {
+    rtm: RTMClient
+  }
+
+  export class SlackAdapter {
+    options: Options
+    client: SlackClient
+  }
+}
