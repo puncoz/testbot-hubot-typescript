f1a6eb7eb012e454c5c46e725022d5c892cff966
Puncoz Nepal <puncoz138@gmail.com>
2019-01-10 23:24:40 +0545

    linter and code style

--- src/scripts/test.ts
+++ src/scripts/test.ts
@@ -1,5 +1,5 @@
-import {Response, Robot} from "hubot"
-import {SlackAdapter} from "hubot-slack"
+import { Response, Robot } from "hubot"
+import { SlackAdapter } from "hubot-slack"
 
 module.exports = async function test(robot: Robot<SlackAdapter>) {
   robot.respond(/test$/i, async (res: Response<SlackAdapter>) => {
